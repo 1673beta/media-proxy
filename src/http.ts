e9d0d20b0e4733b924c7a0aa5a9e0d4a6c35e21c
Esurio <esurio@esurio1673.net>
2025-05-17 08:08:22 +0000

    format

--- src/http.ts
+++ src/http.ts
@@ -1,52 +1,52 @@
-import * as http from 'node:http';
-import * as https from 'node:https';
-import CacheableLookup from 'cacheable-lookup';
-import { HttpProxyAgent, HttpsProxyAgent } from 'hpagent';
-import { LookupFunction } from 'node:net';
+import * as http from "node:http";
+import * as https from "node:https";
+import CacheableLookup from "cacheable-lookup";
+import { HttpProxyAgent, HttpsProxyAgent } from "hpagent";
+import { LookupFunction } from "node:net";
 
 const cache = new CacheableLookup({
-    maxTtl: 3600,	// 1hours
-    errorTtl: 30,	// 30secs
-    lookup: false,	// nativeのdns.lookupにfallbackしない
+	maxTtl: 3600, // 1hours
+	errorTtl: 30, // 30secs
+	lookup: false, // nativeのdns.lookupにfallbackしない
 });
 
 const _http = new http.Agent({
-    keepAlive: true,
-    keepAliveMsecs: 30 * 1000,
-    lookup: cache.lookup as unknown as LookupFunction,
+	keepAlive: true,
+	keepAliveMsecs: 30 * 1000,
+	lookup: cache.lookup as unknown as LookupFunction,
 } as http.AgentOptions);
 
 const _https = new https.Agent({
-    keepAlive: true,
-    keepAliveMsecs: 30 * 1000,
-    lookup: cache.lookup as unknown as LookupFunction,
+	keepAlive: true,
+	keepAliveMsecs: 30 * 1000,
+	lookup: cache.lookup as unknown as LookupFunction,
 } as https.AgentOptions);
 
 export function getAgents(proxy?: string) {
-    const httpAgent = proxy
-        ? new HttpProxyAgent({
-            keepAlive: true,
-            keepAliveMsecs: 30 * 1000,
-            maxSockets: 256,
-            maxFreeSockets: 256,
-            scheduling: 'lifo',
-            proxy: proxy,
-        })
-        : _http;
+	const httpAgent = proxy
+		? new HttpProxyAgent({
+				keepAlive: true,
+				keepAliveMsecs: 30 * 1000,
+				maxSockets: 256,
+				maxFreeSockets: 256,
+				scheduling: "lifo",
+				proxy: proxy,
+			})
+		: _http;
 
-    const httpsAgent = proxy
-        ? new HttpsProxyAgent({
-            keepAlive: true,
-            keepAliveMsecs: 30 * 1000,
-            maxSockets: 256,
-            maxFreeSockets: 256,
-            scheduling: 'lifo',
-            proxy: proxy,
-        })
-        : _https;
+	const httpsAgent = proxy
+		? new HttpsProxyAgent({
+				keepAlive: true,
+				keepAliveMsecs: 30 * 1000,
+				maxSockets: 256,
+				maxFreeSockets: 256,
+				scheduling: "lifo",
+				proxy: proxy,
+			})
+		: _https;
 
-    return {
-        httpAgent,
-        httpsAgent,
-    };
+	return {
+		httpAgent,
+		httpsAgent,
+	};
 }
--- src/image-processor.ts
+++ src/image-processor.ts
@@ -1,46 +1,56 @@
-import sharp from 'sharp';
-import { Readable } from 'node:stream';
+import sharp from "sharp";
+import { Readable } from "node:stream";
 
 export type IImage = {
-    data: Buffer;
-    ext: string | null;
-    type: string;
+	data: Buffer;
+	ext: string | null;
+	type: string;
 };
 
 export type IImageStream = {
-    data: Readable;
-    ext: string | null;
-    type: string;
+	data: Readable;
+	ext: string | null;
+	type: string;
 };
 
 export type IImageStreamable = IImage | IImageStream;
 
 export const webpDefault: sharp.WebpOptions = {
-    quality: 77,
-    alphaQuality: 95,
-    lossless: false,
-    nearLossless: false,
-    smartSubsample: true,
-    mixed: true,
-    effort: 2,
+	quality: 77,
+	alphaQuality: 95,
+	lossless: false,
+	nearLossless: false,
+	smartSubsample: true,
+	mixed: true,
+	effort: 2,
 };
 
-export function convertToWebpStream(path: string, width: number, height: number, options: sharp.WebpOptions = webpDefault): IImageStream {
-    return convertSharpToWebpStream(sharp(path), width, height, options);
+export function convertToWebpStream(
+	path: string,
+	width: number,
+	height: number,
+	options: sharp.WebpOptions = webpDefault,
+): IImageStream {
+	return convertSharpToWebpStream(sharp(path), width, height, options);
 }
 
-export function convertSharpToWebpStream(sharp: sharp.Sharp, width: number, height: number, options: sharp.WebpOptions = webpDefault): IImageStream {
-    const data = sharp
-        .resize(width, height, {
-            fit: 'inside',
-            withoutEnlargement: true,
-        })
-        .rotate()
-        .webp(options)
+export function convertSharpToWebpStream(
+	sharp: sharp.Sharp,
+	width: number,
+	height: number,
+	options: sharp.WebpOptions = webpDefault,
+): IImageStream {
+	const data = sharp
+		.resize(width, height, {
+			fit: "inside",
+			withoutEnlargement: true,
+		})
+		.rotate()
+		.webp(options);
 
-    return {
-        data,
-        ext: 'webp',
-        type: 'image/webp',
-    };
+	return {
+		data,
+		ext: "webp",
+		type: "image/webp",
+	};
 }
