ff9e4eb56d17661319899a67351f4798297149ec
esurio <esurio@esurio1673.net>
2025-05-17 04:28:21 +0000

    remove built file

--- built/http.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" resolution-mode="require"/>
-/// <reference types="node" resolution-mode="require"/>
-import * as http from 'node:http';
-import * as https from 'node:https';
-export declare function getAgents(proxy?: string): {
-    httpAgent: http.Agent;
-    httpsAgent: https.Agent;
-};
